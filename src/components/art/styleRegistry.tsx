674759267185e36810879d1f1069d60de4e32d6c
Charlie Kelly <charliekelly31415@gmail.com>
2025-11-23 23:15:57 -0500

    rename art components

--- src/components/art/styleRegistry.tsx
+++ src/components/art/styleRegistry.tsx
@@ -1,9 +1,9 @@
-import OrbitArt from "./Orbits";
-import StrataArt from "./Strata";
-import ConstellationArt from "./Constellation";
-import BubblesArt from "./Bubbles";
-import WaveArt from "./Wave";
-import SupershapeArt from "./Supershape";
+import OrbitArt from "./OrbitArt";
+import StrataArt from "./StrataArt";
+import ConstellationArt from "./ConstellationArt";
+import BubblesArt from "./BubblesArt";
+import WaveArt from "./WaveArt";
+import SupershapeArt from "./SupershapeArt";
 import type { ArtStyleProps } from "../../logic/types";
 
 export const STYLES: Record<
